9f650a2dc07345d5567ac47dda5c2f1d8ed4285d
Swagata Chaudhuri <swagatachaudhuri@outlook.com>
2022-01-09 10:54:47 +0530

    [Feature] : Project Baseline Commit | v1.0.0
    [Feature] : Project Baseline Commit | v1.0.0
    
    Co-Authored-By: Swagata Chaudhuri <20102710+swagatachaudhuri@users.noreply.github.com>

---
+++ Encryptor/Program.cs
@@ -0,0 +1,79 @@
+﻿Console.WriteLine("Base 64 Encryption / Decryption using .NET");
+Console.WriteLine("******************************************");
+Console.WriteLine();
+int caseDetails = 0;
+do
+{
+    Console.Write("Enter 1 to encrypt, 2 to decrypt & 0 to quit : ");
+    string? inputStr = Console.ReadLine();
+    if (!String.IsNullOrEmpty(inputStr))
+        try
+        {
+            caseDetails = Int32.Parse(inputStr);
+        }
+        catch (FormatException)
+        {
+            caseDetails = -1;
+        }
+    else
+    {
+        caseDetails = -1;
+    }
+    switch (caseDetails)
+    {
+        case 0:
+            Console.WriteLine(Environment.NewLine + "Buh,,byee!!");
+            break;
+        case 1:
+            encrypt();
+            break;
+        case 2:
+            decrypt();
+            break;
+        default:
+            Console.WriteLine(Environment.NewLine + "Wrong Choice Entered!!");
+            break;
+    }
+    Console.WriteLine();
+} while (caseDetails != 0);
+Console.ReadLine();
+
+void encrypt()
+{
+    string encrypted = String.Empty;
+    Console.Write(Environment.NewLine + "Enter string to encrypt : ");
+    string? str = Console.ReadLine();
+    if (str != null)
+    {
+        try
+        {
+            byte[] plainTextBytes = System.Text.Encoding.UTF8.GetBytes(str);
+            encrypted = Convert.ToBase64String(plainTextBytes);
+            Console.WriteLine(Environment.NewLine + encrypted);
+        }
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine(Environment.NewLine + "Input string is not in valid format");
+        }
+    }
+}
+
+void decrypt()
+{
+    string decrypted = String.Empty;
+    Console.Write(Environment.NewLine + "Enter string to decrypt : ");
+    string? str = Console.ReadLine();
+    if (str != null)
+    {
+        try
+        {
+            byte[] base64EncodedBytes = Convert.FromBase64String(str);
+            decrypted = System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+            Console.WriteLine(Environment.NewLine + decrypted);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine(Environment.NewLine + "Input string is not in valid format");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# **Encryptor**
+
+Encryptor is a small dotnet console application that can be used for converting strings to Base64 encoded strings and also to decode Base64 strings to normal string value.
+
+The application can be useful when developers tend to store / configure application secrets in Base64 encoded strings and relying on any 3rd party app is not feasible
+
+---
+
+### **Prerequisites**
+
+.NET Core Runtime Environment (6.0 LTS)
+
+---
+
+### **Running Encryptor**
+
+```python
+cd Encryptor
+
+dotnet run
+```
+
+### **Stopping Encryptor**
+
+```sh
+To close / terminate encryptor app, simple enter 0 as the numeric argument and the application will close
+```
+
+In case you find this repository and code helpful :blush:, don't forget to leave a :star:.
+
+---
